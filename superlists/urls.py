d16371323647c00c5b2d676fdb842e31498f9dc7
idan <idan.nik@gmail.com>
2018-02-03 13:17:56 +0200

    Functional test now checks we can input a to-do item

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,5 +19,5 @@ from lists import views as list_views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', list_views.home_page)
+    path('', list_views.home_page, name='home')
 ]

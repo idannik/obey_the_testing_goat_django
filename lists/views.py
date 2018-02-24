6a7bc437d0c4e076369710508ba24324b802f8e8
Idan Nikritin <idan.nik@gmail.com>
2018-02-24 13:06:58 +0200

    quick test

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from lists.models import Item
 
-
+ 
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])

2321116a4bdd836250ebf8380ad36b36c3499f16
Idan Nikritin <idan.nik@gmail.com>
2018-02-24 13:49:33 +0200

    URLs for forms x2, moved code in views + tests, new URL

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,11 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 
+# TODO: Adjust model so that items are associated with different lists
+# TODO: Add unique URLs for each list …​
+# TODO: Add a URL for creating a new list via POST
+# TODO: Add URLs for adding a new item to an existing list via POST
+
 MAX_WAIT = 10
 
 

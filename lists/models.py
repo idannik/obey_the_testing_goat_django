fe712c4237ef0671f991fdfbf7d2f082e52b73ec
Idan Nikritin <idan.nik@gmail.com>
2018-03-10 18:27:07 +0200

    Use get_absolute_url on List model to DRY urls in views

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-
+from django.urls import reverse
 
 class List(models.Model):
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
     pass
 
 
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -39,7 +39,6 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/{}/'.format(correct_list.id))
         self.assertEqual(response.context['list'], correct_list)
 
-
     def test_can_save_a_POST_request(self):
         self.client.post('/lists/new', data= {
             'item_text': 'A new list item'
@@ -55,6 +54,7 @@ class ListViewTest(TestCase):
         })
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/{}/'.format(new_list.id))
+
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
@@ -69,7 +69,6 @@ class ListViewTest(TestCase):
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
 
-
     def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
@@ -80,7 +79,6 @@ class ListViewTest(TestCase):
         )
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
-
     def test_validation_errors_are_send_back_to_home_page_template(self):
         response = self.client.post('/lists/new', data={'item_text': ''}) # TODO Remove hardcoded URLs from views.py
         self.assertEqual(response.status_code, 200)
@@ -88,8 +86,16 @@ class ListViewTest(TestCase):
         expected_error = escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
 
-
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(f'/lists/{list_.id}/', data={'item_text': ''})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
